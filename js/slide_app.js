859f0031de85cec075f3aaa8ce9a42abf2ed0e10
raj68518 <raj68518@gmail.com>
2020-03-23 21:55:42 +0530

    some id are added in index file and js change

--- js/slide_app.js
+++ js/slide_app.js
@@ -1,5 +1,9 @@
-var sliderImg = document.getElementById('slideImageContainer');
-var slideDotContainer = document.getElementsByClassName('dot_slide')[0];
+const sliderImg = document.getElementById('slideImageContainer');
+const slideDotContainer = document.getElementsByClassName('dot_slide')[0];
+const leftBtn = document.getElementById('left-slide');
+const rightBtn = document.getElementById('right-slide');
+
+let currentIndex = 0;
 
 // creating empty array
 
@@ -10,9 +14,35 @@ imageSrcArray[1] = "../images/slide_img/81swJX.jpg";
 imageSrcArray[2] = "../images/slide_img/images.jpg";
 imageSrcArray[3] = "../images/slide_img/Viuuu-Sneakers.jpeg";
 
+leftBtn.addEventListener('click', ()=>{
+    decIndex();
+});
+
+rightBtn.addEventListener('click', ()=>{
+    ChangeIndex();
+});
 
 
-sliderImg.style.backgroundImage = `url(${imageSrcArray[3]})`;
 
+function ChangeIndex(assignMent) {
+   currentIndex++;
+    if (currentIndex < imageSrcArray.length) {
+            sliderImg.style.backgroundImage = `url(${imageSrcArray[currentIndex]})`;
+     } else {
+            currentIndex = 0;
+            sliderImg.style.backgroundImage = `url(${imageSrcArray[currentIndex]})`;
+    }
+    
 
+}
 
+function decIndex() {
+    if (currentIndex === 0) {
+        currentIndex = imageSrcArray.length -1;
+        sliderImg.style.backgroundImage = `url(${imageSrcArray[currentIndex]})`
+    }else {
+        currentIndex--;
+        sliderImg.style.backgroundImage = `url(${imageSrcArray[currentIndex]})`
+    }
+    
+}
